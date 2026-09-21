a381d87052288d884074bb4e2968bc3e744814ef
luanmaciel55 <contato.luanmaciel@gmail.com>
2026-09-21 18:40:34 -0300

    Conecta site ao Supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,2 @@
+import { createBrowserClient } from '@supabase/ssr';
+export const supabase=createBrowserClient('https://buxevihtjiialecfhkhy.supabase.co','sb_publishable_2UyIhB0in7Fa3pczGkZtzw_NmpvTiBm');
